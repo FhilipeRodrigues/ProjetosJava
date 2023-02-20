2868366a247d0cc517f082dfe59d6e4ef28bf00c
Fhilipe Rodrigues Dantas <122138231+ferrugi@users.noreply.github.com>
2023-02-20 12:19:56 -0300

    Add files via upload

---
+++ IdiomaSistema.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.idiomasistema;
+import java.util.*;
+/**
+ *
+ * @author TM User
+ */
+public class IdiomaSistema {
+
+    public static void main(String[] args) {
+        Locale loc = Locale.getDefault();
+        System.out.println(loc.getDisplayLanguage());
+        System.out.println(loc.getLanguage());
+    }
+}
